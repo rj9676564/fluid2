ebdb2294bf9b3fc76829562ec071a1ba335c5390
rj9676564 <laibin6@gmail.com>
2024-06-16 21:52:28 +0800

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,38 @@
+import {
+  pg_colors,
+  pg_fonts,
+  pg_backgrounds,
+} from './themes/pg-tailwindcss/tokens.mjs'
+
+import { getFontsWithFallback } from './app/utils/font'
+import { safelist } from './app/utils/colors'
+
+export default {
+  darkMode: 'class',
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@pinegrow/tailwindcss-plugin').default({
+      colors: pg_colors, // primary, secondary etc
+      fonts: getFontsWithFallback(pg_fonts),
+      backgrounds: pg_backgrounds, // bg-design-image, bg-design-image-large
+    }),
+  ],
+
+  safelist,
+
+  get content() {
+    const _content = [
+      '{.,app,*-layer}/components/**/*.{js,vue,ts}',
+      '{.,app,*-layer}/layouts/**/*.vue',
+      '{.,app,*-layer}/pages/**/*.vue',
+      '{.,app,*-layer}/plugins/**/*.{js,ts}',
+      '{.,app,*-layer}/app.vue',
+      '{.,app,*-layer}/*.{mjs,js,ts}',
+      '{.,*-layer}/nuxt.config.{js,ts}',
+    ]
+    return process.env.NODE_ENV === 'production'
+      ? _content
+      : [..._content, './_pginfo/**/*.{html,js}'] // used by Vue Desginer for live-designing during development
+  },
+}
